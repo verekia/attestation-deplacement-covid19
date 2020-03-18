2ed5ef9449200fd6343d49a7c9e5cc12cc8b831a
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-03-18 09:33:05 +0100

    Signature, basic styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import Helmet from 'react-helmet'
+import SignatureCanvas from 'react-signature-canvas'
 
-import { Global } from '@emotion/core'
+import '../styles.css'
 
 const IndexPage = () => (
   <div>
-    <Global styles={{ html: { fontFamily: 'Roboto' }, body: { width: 800, margin: '0 auto' } }} />
     <Helmet>
       <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap"
+        rel="stylesheet"
+      ></link>
     </Helmet>
     <h1>ATTESTATION DE DÉPLACEMENT DÉROGATOIRE</h1>
     <p>
@@ -16,9 +20,21 @@ const IndexPage = () => (
       déplacements dans le cadre de la lutte contre la propagation du virus Covid-19 :
     </p>
     <p>Je soussigné(e)</p>
-    <p>Mme / M.</p>
-    <p>Né(e) le :</p>
-    <p>Demeurant :</p>
+    <div>
+      <p style={{ float: 'left', width: '20%' }}>Mme / M.</p>
+      <textarea style={{ float: 'right', width: '80%' }}></textarea>
+    </div>
+    <div style={{ clear: 'both' }} />
+    <div>
+      <p style={{ float: 'left', width: '20%' }}>Né(e) le :</p>
+      <textarea style={{ float: 'right', width: '80%' }}></textarea>
+    </div>
+    <div style={{ clear: 'both' }} />
+    <div>
+      <p style={{ float: 'left', width: '20%' }}>Demeurant :</p>
+      <textarea style={{ float: 'right', width: '80%' }}></textarea>
+    </div>
+    <div style={{ clear: 'both' }} />
     <p>
       certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par l’article
       1er du décret du 16 mars 2020 portant réglementation des déplacements dans le cadre de la
@@ -26,6 +42,7 @@ const IndexPage = () => (
     </p>
     <form>
       <p>
+        <input type="checkbox" />
         déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle,
         lorsqu’ils sont indispensables à l’exercice d’activités ne pouvant être organisées sous
         forme de télétravail (sur justificatif permanent) ou déplacements professionnels ne pouvant
@@ -33,26 +50,39 @@ const IndexPage = () => (
       </p>
 
       <p>
+        <input type="checkbox" />
         déplacements pour effectuer des achats de première nécessité dans des établissements
         autorisés (liste sur gouvernement.fr) ;
       </p>
 
-      <p>déplacements pour motif de santé ;</p>
+      <p>
+        <input type="checkbox" /> déplacements pour motif de santé ;
+      </p>
 
       <p>
+        <input type="checkbox" />
         déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou
         la garde d’enfants ;
       </p>
 
       <p>
+        <input type="checkbox" />
         déplacements brefs, à proximité du domicile, liés à l’activité physique individuelle des
         personnes, à l’exclusion de toute pratique sportive collective, et aux besoins des animaux
         de compagnie.
       </p>
     </form>
 
-    <p>Fait à ................................., le......../......../2020</p>
+    <p>
+      Fait à <input type="text" style={{ width: 200 }} />, le{' '}
+      <input type="text" style={{ width: 30 }} />/<input type="text" style={{ width: 30 }} />
+      /2020
+    </p>
     <p>(signature)</p>
+    <SignatureCanvas
+      penColor="#3d3de6"
+      canvasProps={{ width: 500, height: 200, className: 'sigCanvas' }}
+    />
   </div>
 )
 
